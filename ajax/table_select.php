014baee95381efaaeb80b0f35b6e8e3f3a6f2a47
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-06-01 18:34:39 +0200

    debug

--- ajax/table_select.php
+++ ajax/table_select.php
@@ -11,10 +11,12 @@ $table = GETPOST('table_add');
 $color = '#aad4ff';
 $color2= '#56aaff';
 
+$db->DDLInfoTable($table);
 
 $return = '<div class="cal_event cal_event_busy" align="left" draggable="false" style="background:' . $color .'; background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.'));';
 $return.=  'border-radius:6px; margin-bottom: 3px; width:200px;">';
-$return.= $table;
+$return.= '<a href="" onclick="javascript:visibilite(\'' . $table . '\'); return false;" >'. img_edit_add('+','') . '<h style="font-size: large; color: white;"><b>' . $table . ' </b></h></a>';
+$return.= '  '. $table;
 $return.= '</div>';
 
 echo $return;
