5eebd00e1a49e9ed9b4bfe2ffb3f91cedb7d33ed
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-06-01 18:28:09 +0200

    debug

--- ajax/table_select.php
+++ ajax/table_select.php
@@ -8,8 +8,8 @@ if (! $res)
 global $db, $user;
 
 $table = GETPOST('table_add');
-$color = '#56ff56';
-$color2= '#00ff00';
+$color = '#aad4ff';
+$color2= '#56aaff';
 
 
 $return = '<div class="cal_event cal_event_busy" align="left" draggable="false" style="background:' . $color .'; background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.'));';
