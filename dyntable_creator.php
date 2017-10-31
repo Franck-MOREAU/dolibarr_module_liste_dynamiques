799a459544a0d86710f2a1b1bbf98b75c9ac728e
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-10-31 11:35:28 +0100

    debug

--- dyntable_creator.php
+++ dyntable_creator.php
@@ -462,7 +462,7 @@ document.getElementById("table").onchange = function(){
  		},
  		success: function(msg){
  			if (msg != ""){
- 				$('div.fiche div.table_in').first().prepend(msg);
+ 				$('div.fiche div.table_in').last().prepend(msg);
  			}
  		},
  		error: function(msg){
